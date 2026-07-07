624d4b874003523622f0d6af4cc7d5bb0c540698
kinhodevjr <kinho1206@bol.com.br>
2026-07-07 16:37:36 -0300

    frontend com interface do login, integrado com backend

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,10 +1,12 @@
 require("dotenv").config()
 var express = require("express");
+const cors = require("cors");
 var app = express();
 //permite em json
 app.use(express.json());
 //crio a rota, usando APP.(metodo de https, POST,GET,DELETE), app.post("/", )() =>{}) 
 // express (ROTA)
+app.use(cors());
 app.use(require("./routes/listar_users"))
 app.use(require("./routes/cadastro"))
 app.use(require("./routes/buscar_user"))
--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,122 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import "./App.css";
+import { useState } from "react";
+function App(){
+  const [login, setLogin] = useState("");
+  const [senha, setSenha] =useState("");
 
-function App() {
-  const [count, setCount] = useState(0)
 
+  async function user_login (){
+    console.log(login)
+    console.log (senha)
+    const api = await fetch("http://localhost:3000/login", {
+      method: "POST",
+      headers:{
+        "content-Type":"application/json"
+      },
+      body: JSON.stringify({
+        login: login,
+        senha: senha
+      }),
+      
+    });
+    const dados = await api.json()
+    console.log(dados)
+  }
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+      
+    <div>
+    <h1> Sistema Estrelar </h1>
+    
+    <p>ESTRELAR CONSULTORIA</p>
 
-      <div className="ticks"></div>
+    <label>USUARIO: </label><br/>
+    <input type="text" placeholder="LOGIN" onChange={(event) =>{
+      setLogin(event.target.value)
+    }}/><br/>
+    <label>SENHA: </label><br/>
+    <input type="password" placeholder="SENHA" onChange={(event) =>{
+      setSenha(event.target.value)
+    }}/><br />
+    <button onClick={user_login}>ENTRAR</button>
+    
+    
+    
+    </div>
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+  );
 }
-
-export default App
+export default App;
